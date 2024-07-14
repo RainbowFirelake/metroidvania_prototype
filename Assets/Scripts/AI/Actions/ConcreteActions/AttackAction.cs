55abcea76b6e9b219c9acd6c3b874417eb7e2a2e
RainbowFire <45136883+RainbowFirelake@users.noreply.github.com>
2024-07-15 01:16:00 +0700

    fix: compilation error after unexpected vs closing

--- Assets/Scripts/AI/Actions/ConcreteActions/AttackAction.cs
+++ Assets/Scripts/AI/Actions/ConcreteActions/AttackAction.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assets.Scripts.AI.Actions.ConcreteActions
+namespace Metroidvania.AI.Actions
 {
-    internal class AttackAction
+    public class AttackAction : IAction
     {
+        public void Execute()
+        {
+
+        }
     }
 }
--- Assets/Scripts/AI/ConcreteBehaviours/AttackStrategy.cs
+++ Assets/Scripts/AI/ConcreteBehaviours/AttackStrategy.cs
@@ -1,10 +1,22 @@
-﻿namespace Metroidvania.AI.ConcreteBehaviours
+﻿using Metroidvania.AI.BehaviorTrees;
+
+namespace Metroidvania.AI.ConcreteBehaviours
 {
-    public class AttackStrategy
+    public class AttackStrategy : IBehaviourStrategy
     {
         public AttackStrategy()
         {
 
+        }
+
+        public Node.Status Process()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
